2298b56920c67fed6320cd089b1b2c03ec5607ab
Jamal Ait Chdid <jait-chd@c2r3p14.1337.ma>
2025-07-02 21:44:48 +0100

    iam starting now on built ins i already create like env and exit but there is a lot of things to handle on these 2 files

--- execution/is_built_in.c
+++ execution/is_built_in.c
@@ -1,13 +1,31 @@
 #include "exec.h"
 
 
+void exit_b_in(char *line) {
+    printf("exit\n");
+    exit(0);
+}
+
+void env_b_in(char **env) {
+    int i = 0;
+    while(env[i]) {
+        printf("%s/n",env[i]);
+        i++;
+    }
+}
+
+
+
+
+
+// this function check if is a cmd built then i will execute it on another way
 void is_built_in(char *line , char **env) {
     if(ft_strncmp(line , "exit" , 4) == 0)
         exit_b_in(line);
     else if(ft_strncmp(line , "echo" , 4) == 0)
         echo_b_in(line);
     else if(ft_strncmp(line , "env" , 3) == 0)
-        env_b_in(line);
+        env_b_in(env);
     else if(ft_strncmp(line , "unset" , 5) == 0)
         unset_b_in(line);
     else if(ft_strncmp(line , "export" , 6) == 0)
@@ -16,5 +34,4 @@ void is_built_in(char *line , char **env) {
         cd_b_in(line);
     else if(ft_strncmp(line , "pwd" , 3) == 0)
         pwd_b_in(line);
-
 }
