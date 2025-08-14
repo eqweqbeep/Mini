fe8d2c57d280099b4bb018aaafc50f60dfb61d6e
eqweqbeep <kiloxm4sec@gmail.com>
2025-08-14 12:50:35 +0100

    Handle pipelines across multiple commands

--- main_program/utils.c
+++ main_program/utils.c
@@ -12,63 +12,28 @@
 
 #include "minishell.h"
 
-// size_t ft_strlen(const char *s) {
-//     size_t i = 0;
-//     while (s[i])
-//         i++;
-//     return i;
-// }
+char *ft_strdup(const char *s)
+{
+    size_t  i;
+    char    *cpy;
 
-char *ft_substr(char const *s, unsigned int start, size_t len) {
-    size_t i = 0;
-    char *str;
-    size_t s_len = ft_strlen(s);
-
-    if (!s)
-        return 0;
-    if (start >= s_len)
-        return ft_strdup("");
-    if (len < s_len - start)
-        str = malloc(len + 1);
-    else
-        str = malloc(s_len - start + 1);
-    if (!str)
-        return 0;
-    while (i < len && s[start])
-        str[i++] = s[start++];
-    str[i] = 0;
-    return str;
-}
-
-int ft_strncmp(const char *s1, const char *s2, size_t n) {
-    size_t i = 0;
-    if (n == 0)
-        return 0;
-    while ((i < n) && (s1[i] || s2[i])) {
-        if (s1[i] != s2[i])
-            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-        i++;
-    }
-    return 0;
-}
-
-char *ft_strdup(const char *s) {
-    size_t i = 0;
-    char *cpy = malloc(ft_strlen(s) + 1);
+    i = 0;
+    cpy = malloc(ft_strlen((char *)s) + 1);
     if (!cpy)
-        return NULL;
-    while (s[i]) {
+        return (NULL);
+    while (s[i])
+    {
         cpy[i] = s[i];
         i++;
     }
-    cpy[i] = 0;
-    return cpy;
+    cpy[i] = '\0';
+    return (cpy);
 }
 
-void ft_putstr_fd(char *s, int fd)
+void    ft_putstr_fd(char *s, int fd)
 {
     if (!s)
-        return;
+        return ;
     while (*s)
     {
         write(fd, s, 1);
@@ -76,10 +41,10 @@ void ft_putstr_fd(char *s, int fd)
     }
 }
 
-void ft_putendl_fd(char *s, int fd)
+void    ft_putendl_fd(char *s, int fd)
 {
     if (!s)
-        return;
+        return ;
     ft_putstr_fd(s, fd);
     write(fd, "\n", 1);
 }
