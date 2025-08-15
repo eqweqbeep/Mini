cc4b6991f00c3443db9f761655a7f6903d69963c
eqweqbeep <kiloxm4sec@gmail.com>
2025-08-15 21:24:55 +0100

    Add export and unset builtins with robust exit status

--- builtins/ft_pwd.c
+++ builtins/ft_pwd.c
@@ -10,35 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "builtins.h"
 
-int	ft_pwd(void)
+int     ft_pwd(char **args)
 {
-	char	cwd[1024];
+        char    cwd[1024];
 
-	if (getcwd(cwd, sizeof(cwd)))
-	{
-		write(1, cwd, strlen(cwd));
-		write(1, "\n", 1);
-		// exit(0);
-	}
-	else
-		perror("pwd");
-	return (0);
+        if (args[1])
+        {
+                write(2, "minishell: pwd: too many arguments\n", 35);
+                return (1);
+        }
+        if (getcwd(cwd, sizeof(cwd)))
+        {
+                write(1, cwd, strlen(cwd));
+                write(1, "\n", 1);
+                return (0);
+        }
+        perror("pwd");
+        return (1);
 }
- // pwd + arg must ignore all options next to pwd and affiche pwd
-
- // mkdir test and remove it on another terminal and test pwd
-//  mkdir test_dir
-// cd test_dir
-// chmod 000 .
-// pwd
-// # Should still work (pwd doesn't need read permission)
-// some test cases with symbolic links
-//13. PWD in Pipelines
-//14. PWD with Redirections
-//15. PWD in Subshells
-//16. PWD in Command Substitution
-//18. PWD When $PWD is Modified
-// 19. PWD When $PWD is Unset
