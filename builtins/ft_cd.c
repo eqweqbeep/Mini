6ebcb83120090181387c38a31905819fd8f0fc31
eqweqbeep <kiloxm4sec@gmail.com>
2025-08-15 21:11:05 +0100

    Add export and unset builtins with robust exit status handling

--- builtins/ft_cd.c
+++ builtins/ft_cd.c
@@ -6,28 +6,52 @@
 /*   By: jait-chd <jait-chd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 21:46:08 by jait-chd          #+#    #+#             */
-/*   Updated: 2025/07/13 21:54:53 by jait-chd         ###   ########.fr       */
+/*   Updated: 2025/08/13 06:57:00 by jait-chd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "builtins.h"
+#include "../main_program/minishell.h"
 
-int	ft_cd(char **args)
+static char *get_home(char **env)
 {
-	if (!args[1])
-	{
-		write(2, "[cd] : is only a shortcut for cd /home so\n", 42);
-		 return (1);
-		// the exit status should be 0
-		 exit(0);
-	}
-	if (chdir(args[1]) != 0)
-	{
-		write(2,"No such file or directory\n" , 27);
-		return (1);
-		exit(0);
-	}
-	return (0);
+        return (get_env_value(env, "HOME"));
 }
-// case 01 chmod folder to 000 and try to enter to it 
+
+int     ft_cd(char **args, char ***env)
+{
+        char    *path;
+        char    oldpwd[1024];
+        char    newpwd[1024];
+
+        if (args[1] && args[2])
+        {
+                write(2, "minishell: cd: too many arguments\n", 34);
+                return (1);
+        }
+        if (!args[1])
+        {
+                path = get_home(*env);
+                if (!path)
+                {
+                        write(2, "minishell: cd: HOME not set\n", 28);
+                        return (1);
+                }
+        }
+        else
+                path = args[1];
+        if (!getcwd(oldpwd, sizeof(oldpwd)))
+                oldpwd[0] = '\0';
+        if (chdir(path) != 0)
+        {
+                write(2, "minishell: cd: ", 16);
+                perror(path);
+                return (1);
+        }
+        if (getcwd(newpwd, sizeof(newpwd)))
+        {
+                set_env_var(env, "OLDPWD", oldpwd);
+                set_env_var(env, "PWD", newpwd);
+        }
+        return (0);
+}
